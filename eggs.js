6c81672c2f68dcc76126ff933829af57e57e56e2
Vivian Rose <26861054+Roachbones@users.noreply.github.com>
2025-12-13 16:52:10 -0600

    First draft

---
+++ eggs.js
@@ -0,0 +1,63 @@
+// Tiny little features nobody will notice, tucked away here so as not to clutter the mains.
+
+function invalid_guess_egg_message(guess) {
+    if (guess == 'help') {
+        rules.open = true; return ' ';
+    }
+    if (guess == 'hint') {
+        return choice(['Try thinking of ']) + choice(['bugs','farm animals','pests','dinosaurs','fish. Many fish names just end in -fish']) + '.';
+    }
+    if (guess == 'dragon') {
+        return 'Real animals only, please.';
+    }
+    if (guess == 'jumping bean') {
+        return "Well, the jumping bean is the moth larva, but it's also the seed pod. So I don't think you can say that a jumping bean is itself an animal. But I'd definitely accept “jumping bean moth”.";
+    }
+}
+
+function valid_guess_egg_message(guess, guess_id) {
+    if (guess == 'unicorn') {
+        return "You probably didn't mean the genus of goblin spider named after its characteristic pointed facial projection, but whatever, sure.";
+    }
+    if (guess == 'sphinx') {
+        return 'You mean sphinx moths, right? Not the mythical riddlers?';
+    }
+    if (guess == 'mule') {
+        return 'Kind of a weird case, but sure, it counts.';
+    }
+    if (guess == 'killer hornet') {
+        return "Okay, sure, I'll allow it, but you should really just call it the Asian giant hornet.";
+    }
+    /* if (guess.includes('skeleton')) { return 'You feel your sins crawling on your back.'; } */
+    if (guess_id == 'Q15978631') { return "That's me!"; } // human
+    if (guess_id == 'Q1947892') { return "Don't you love their songs?" } // cicada
+}
+
+function egg_manipulate_li(li, guess, guess_id) {
+    if (guess == 'longcat') {
+        li.style.scale = '3 1';
+    }
+}
+
+HUMAN_EMOJI = `👩‍❤️‍💋‍👩
+👩‍❤️‍👩
+🥷
+🤡
+💁
+🤷
+👩‍💻
+🫅
+🧍
+👤
+👯
+🕴️
+🧑‍🌾
+🧑‍🔬
+🤸
+🧓
+👧
+🧑‍🎓`.split('\n')
+
+HUMAN = 'Q15978631'
+MONONYMS[HUMAN] = MONONYMS[HUMAN] || [];
+MONONYMS[HUMAN].push(...HUMAN_EMOJI);
